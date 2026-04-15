b95a3b56c9973abbbdeffbe639fab654464856b1
mdabutaher4075-netizen <md.abutaher4075@gmail.com>
2026-04-15 13:53:42 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# সিস্টেমের প্রয়োজনীয় গ্রাফিক্স লাইব্রেরি ইনস্টল করা
+sudo apt-get update
+sudo apt-get install -y libnss3 libgbm1 libasound2 libxshmfence1 libx11-xcb1 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxcomposite1 libxdamage1 libxrandr2 libpango-1.0-0 libpangocairo-1.0-0
